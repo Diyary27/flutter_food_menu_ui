76c7b339ed59eded63431400744b53d5dc770d7b
main <naozary.diyar@gmail.com>
2024-01-28 08:53:43 -0800

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFFFC20D);
+const kAccentColor = Color(0XFFF2B501);
+
+const kDarkColor = Color(0XFF212121);
+const kDarkGreyColor = Color(0XFF2C2C2C);
+const kLightGreyColor = Color(0XFF272727);
+const kBottomColor = Color(0XFF0D0D0D);
+
+const kLightFontColor = Color(0XFFF3F3F3);
+const kLightGreyFontColor = Color(0XFFC6C5C5);
+const kDarkGreyFontColor = Color(0XFF8E8E8E);
+
+const kRedColor = Color(0XFFFF3D3D);
+const kLightRedColor = Color(0xFF8D2929);
